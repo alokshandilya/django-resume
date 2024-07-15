d072ca841ac8c259417f65109526f9ada7a96428
alokshandilya <alok.shandilya10@gmail.com>
2024-07-15 18:23:17 +0530

    add forms

---
+++ main/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import ContactProfile
+
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "*Full name..",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        max_length=254,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "*Email..",
+            }
+        ),
+    )
+    message = forms.CharField(
+        max_length=1000,
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "*Message..",
+                "class": "form-control",
+                "rows": 6,
+            }
+        ),
+    )
+
+    class Meta:
+        model = ContactProfile
+        fields = (
+            "name",
+            "email",
+            "message",
+        )
